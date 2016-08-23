11fdf6f7aff0f040bbadab2afd933ec5927072b0
SOMA SHEKAR NAGANNA <soma@Somas-MacBook-Pro.local>
2016-08-23 10:04:21 +0530

    updated updateAddress

--- ibm-ciav-services.go
+++ ibm-ciav-services.go
@@ -370,24 +370,48 @@ func UpdateAddress(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
   poaDoc := args[10]
   expiryDate := args[11]
   source := args[12]
+   stub.
+	// ok, err := stub.ReplaceRow("Address", shim.Row{
+	// 	Columns: []*shim.Column{
+  //     &shim.Column{Value: &shim.Column_String_{String_: customerId}},
+  //     &shim.Column{Value: &shim.Column_String_{String_: addressId}},
+  //     &shim.Column{Value: &shim.Column_String_{String_: addressType}},
+  //     &shim.Column{Value: &shim.Column_String_{String_: doorNumber}},
+  //     &shim.Column{Value: &shim.Column_String_{String_: street}},
+  //     &shim.Column{Value: &shim.Column_String_{String_: locality}},
+	// 		&shim.Column{Value: &shim.Column_String_{String_: city}},
+  //     &shim.Column{Value: &shim.Column_String_{String_: state}},
+  //     &shim.Column{Value: &shim.Column_String_{String_: pincode}},
+  //     &shim.Column{Value: &shim.Column_String_{String_: poaType}},
+  //     &shim.Column{Value: &shim.Column_String_{String_: poaDoc}},
+  //     &shim.Column{Value: &shim.Column_String_{String_: expiryDate}},
+  //     &shim.Column{Value: &shim.Column_String_{String_: source}},
+	// 	},
+	// })
 
-	ok, err := stub.ReplaceRow("Address", shim.Row{
-		Columns: []*shim.Column{
-      &shim.Column{Value: &shim.Column_String_{String_: customerId}},
-      &shim.Column{Value: &shim.Column_String_{String_: addressId}},
-      &shim.Column{Value: &shim.Column_String_{String_: addressType}},
-      &shim.Column{Value: &shim.Column_String_{String_: doorNumber}},
-      &shim.Column{Value: &shim.Column_String_{String_: street}},
-      &shim.Column{Value: &shim.Column_String_{String_: locality}},
-			&shim.Column{Value: &shim.Column_String_{String_: city}},
-      &shim.Column{Value: &shim.Column_String_{String_: state}},
-      &shim.Column{Value: &shim.Column_String_{String_: pincode}},
-      &shim.Column{Value: &shim.Column_String_{String_: poaType}},
-      &shim.Column{Value: &shim.Column_String_{String_: poaDoc}},
-      &shim.Column{Value: &shim.Column_String_{String_: expiryDate}},
-      &shim.Column{Value: &shim.Column_String_{String_: source}},
-		},
-	})
+	var columns []shim.Column
+	col1 := shim.Column{Value: &shim.Column_String_{String_: customerId}}
+	columns = append(columns, col1)
+
+stub.DeleteRow("Address", columns)
+
+ok, err := stub.InsertRow("Address", shim.Row{
+	Columns: []*shim.Column{
+		&shim.Column{Value: &shim.Column_String_{String_: customerId}},
+		&shim.Column{Value: &shim.Column_String_{String_: addressId}},
+		&shim.Column{Value: &shim.Column_String_{String_: addressType}},
+		&shim.Column{Value: &shim.Column_String_{String_: doorNumber}},
+		&shim.Column{Value: &shim.Column_String_{String_: street}},
+		&shim.Column{Value: &shim.Column_String_{String_: locality}},
+		&shim.Column{Value: &shim.Column_String_{String_: city}},
+		&shim.Column{Value: &shim.Column_String_{String_: state}},
+		&shim.Column{Value: &shim.Column_String_{String_: pincode}},
+		&shim.Column{Value: &shim.Column_String_{String_: poaType}},
+		&shim.Column{Value: &shim.Column_String_{String_: poaDoc}},
+		&shim.Column{Value: &shim.Column_String_{String_: expiryDate}},
+		&shim.Column{Value: &shim.Column_String_{String_: source}},
+	},
+})
 
 	if !ok && err == nil {
 		return nil, errors.New("Error in updated customer address record.")
